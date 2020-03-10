637a5553577fa37083d8b0d5a158fe665ff51c0e
Andriani <valerie.andriani@gmail.com>
2020-03-10 22:42:48 +0100

    Update Chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,17 +3,13 @@ import React, { Component } from 'react';
 class Chat extends Component {
     render() {
         return (
-            <h1>My Chat App</h1>
-            <form style="width: 100%">
-                <div class="bar bar-header item-input-inset">
-                    <label class="item-input-wrapper">
-                        <input type="search" placeholder="Tapez un message" ng-model="add.message" />
-                    </label>
-                    <button class="button button-clear" type="submit" ng-click="add(add)">
-                    Envoyer
-                    </button>
+            <div class="chat__type__body">
+                <h1>My Chat App</h1>
+                <div class="chat__type">
+                    <textarea id="msg_box" placeholder="Type your message"></textarea>
+                    <button class="btn btn-primary" id="sendMessage">Send</button>
                 </div>
-            </form>
+            </div>
         )
     }
 }
