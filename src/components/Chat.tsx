b30750b122cca870869a3af8baa45bc6d47ba729
Andriani <valerie.andriani@gmail.com>
2020-04-12 14:37:12 +0200

    Styled Chat component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
+const textArea = {
+    borderRadius: 8,
+    width: 450,
+    height: 250
+};
+
+const styled = {
+    'display': 'flex',
+    'flex-direction': 'column',
+    'justify-content': 'space-evenly'
+}
+
+
 class Chat extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="chat__type__body">
+                <h1>My Chat App</h1>
+                <div className="chat__type" style={styled}>
+                    <div>
+                        <textarea style={textArea} id="msg_box" placeholder="Type your message"></textarea>
+                    </div>
+                    <div>
+                        <button className="btn btn-primary" id="sendMessage">Send</button>
+                    </div>
+                </div>
             </div>
         );
     }
